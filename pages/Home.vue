4f48937473dc8a62e0f92e46a7b1388d0094f26b
Jason Kiptoo <jason@nathandigital.com>
2025-01-29 09:39:48 +0300

    morning fixes

--- pages/Home.vue
+++ pages/Home.vue
@@ -6,9 +6,11 @@
         <div class="badge text-orange-500 text-sm font-semibold mb-2">
           100% Best Product
         </div>
-        <h1 class="text-4xl font-bold mb-4">HD Bullet Camera </h1>
-        <p class="text-gray-500 text-lg mb-6">Wide Angle </p>
-        <div class="price flex items-center text-2xl font-semibold text-red-600 mb-4">
+        <h1 class="text-4xl font-bold mb-4">HD Bullet Camera</h1>
+        <p class="text-gray-500 text-lg mb-6">Wide Angle</p>
+        <div
+          class="price flex items-center text-2xl font-semibold text-red-600 mb-4"
+        >
           {{ price }}
           <span class="line-through text-gray-400 text-lg ml-4">KES 2,860</span>
         </div>
@@ -30,49 +32,93 @@
             </div>
           </div>
         </div>
-        <button class="px-6 py-3 bg-orange-500 text-white rounded-lg shadow-lg hover:bg-orange-600">
+        <button
+          class="px-6 py-3 bg-orange-500 text-white rounded-lg shadow-lg hover:bg-orange-600"
+        >
           SHOP NOW →
         </button>
       </div>
       <!-- Dynamic Image Section -->
-      <div class="dynamic-image mt- flex justify-center items-center relative w-1/3">
-        <img :src="selectedImage" alt="Selected Product" class="w-full h-full rounded- shadow- border-0" />
-        <div class="absolute bottom-0 left-0 text-white bg-orange-500 text-sm px-3 py-1 rounded-full"
-          style="transform: translate(-50%, 50%)">
+      <div
+        class="dynamic-image mt- flex justify-center items-center relative w-1/3"
+      >
+        <img
+          :src="selectedImage"
+          alt="Selected Product"
+          class="w-full h-full rounded- shadow- border-0"
+        />
+        <div
+          class="absolute bottom-0 left-0 text-white bg-orange-500 text-sm px-3 py-1 rounded-full"
+          style="transform: translate(-50%, 50%)"
+        >
           29% off
         </div>
       </div>
       <!-- Right Section -->
       <div class="w-1/3 pa-6 ma-5 pl-40 h-40 dark:border-orange-600">
-        <div class="border border-red-600 dark:border-orange-600 rounded m-2 p-4">
+        <div
+          class="border border-red-600 dark:border-orange-600 rounded m-2 p-4"
+        >
           <!-- Static Content -->
-          <div class="mb-4 font-medium text-dark-600 text-center">Top Product</div>
+          <div class="mb-4 font-medium text-dark-600 text-center">
+            Top Product
+          </div>
 
           <!-- Carousel for Dynamic Content -->
-          <Carousel :d_circular="true" :autoplayInterval="3000" :value="products" :numVisible="1" :numScroll="1"
-            orientation="horizontal" containerClass="flex items-center" :showIndicators="false" :showNavigators="false">
+          <Carousel
+            :d_circular="true"
+            :autoplayInterval="3000"
+            :value="products"
+            :numVisible="1"
+            :numScroll="1"
+            orientation="horizontal"
+            containerClass="flex items-center"
+            :showIndicators="false"
+            :showNavigators="false"
+          >
             <template #item="slotProps">
               <!-- Dynamic Content -->
               <div>
                 <div class="mb-4">
                   <div class="relative mx-auto">
-                    <img :src="slotProps.data.image" :alt="slotProps.data.name" class="object-cover rounded"
-                      style="max-width: 100%;" />
-                    <Tag :value="slotProps.data.inventoryStatus" :severity="getSeverity(slotProps.data.inventoryStatus)"
-                      class="absolute" style="left:5px; top: 5px" />
+                    <img
+                      :src="slotProps.data.image"
+                      :alt="slotProps.data.name"
+                      class="object-cover rounded"
+                      style="max-width: 100%"
+                    />
+                    <Tag
+                      :value="slotProps.data.inventoryStatus"
+                      :severity="getSeverity(slotProps.data.inventoryStatus)"
+                      class="absolute"
+                      style="left: 5px; top: 5px"
+                    />
                   </div>
                 </div>
 
-                <div class="mb-4 font-medium flex justify-around">{{ slotProps.data.name }}</div>
-                <div class="flex justify-between items-center flex justify-around">
-                  <div class="mt-0 font-semibold text-xl flex justify-around py-4">{{ slotProps.data.price }}</div>
+                <div class="mb-4 font-medium flex justify-around">
+                  {{ slotProps.data.name }}
+                </div>
+                <div
+                  class="flex justify-between items-center flex justify-around"
+                >
+                  <div
+                    class="mt-0 font-semibold text-xl flex justify-around py-4"
+                  >
+                    {{ slotProps.data.price }}
+                  </div>
                 </div>
 
-                <div class="stock flex items-center text-sm text-gray-600 mb-6 flex justify-around">
+                <div
+                  class="stock flex items-center text-sm text-gray-600 mb-6 flex justify-around"
+                >
                   <div class="w-full max-w-xs">
                     <div class="relative">
                       <div class="h-2 bg-gray-300 rounded">
-                        <div class="h-2 bg-orange-500 rounded" style="width: 65%"></div>
+                        <div
+                          class="h-2 bg-orange-500 rounded"
+                          style="width: 65%"
+                        ></div>
                       </div>
                       <div class="flex justify-between text-xs mt-2">
                         <span>Available: 334</span>
@@ -86,30 +132,43 @@
           </Carousel>
         </div>
       </div>
-
     </div>
 
     <!-- Carousel Section -->
     <div class="carousel flex justify-center items-center mt-8 space-x-4">
-      <button @click="prevImage" class="text-gray-600 hover:text-gray-800 px-3 py-2 bg-gray-100 rounded-full shadow-md">
+      <button
+        @click="prevImage"
+        class="text-gray-600 hover:text-gray-800 px-3 py-2 bg-gray-100 rounded-full shadow-md"
+      >
         ←
       </button>
       <div class="flex items-center space-x-4">
-        <img v-for="(image, index) in images" :key="index" :src="image" :alt="'Product ' + (index + 1)"
+        <img
+          v-for="(image, index) in images"
+          :key="index"
+          :src="image"
+          :alt="'Product ' + (index + 1)"
           @click="selectImage(image)"
           class="w-16 h-16 rounded-md border border-gray-300 cursor-pointer hover:border-orange-500"
-          :class="{ 'border-orange-500': selectedImage === image }" />
+          :class="{ 'border-orange-500': selectedImage === image }"
+        />
       </div>
-      <button @click="nextImage" class="text-gray-600 hover:text-gray-800 px-3 py-2 bg-gray-100 rounded-full shadow-md">
+      <button
+        @click="nextImage"
+        class="text-gray-600 hover:text-gray-800 px-3 py-2 bg-gray-100 rounded-full shadow-md"
+      >
         →
       </button>
     </div>
 
     <div class="feature__wrap grid grid-cols-5 gap-2 m-12">
-      <div class="feature__item flex flex-col items-center text-center" v-for="(feature, index) in features"
-        :key="index">
+      <div
+        class="feature__item flex flex-col items-center text-center"
+        v-for="(feature, index) in features"
+        :key="index"
+      >
         <div class="icon">
-          <i :class="feature.icon" style="font-size: 2rem;color:#f44336"></i>
+          <i :class="feature.icon" style="font-size: 2rem; color: #f44336"></i>
         </div>
         <div class="content mt-4">
           <h3 class="text-lg font-semibold">{{ feature.title }}</h3>
@@ -122,10 +181,10 @@
 </template>
 
 <script>
-import CardsHome from "~/components/cards/home.vue"
+import CardsHome from "~/components/cards/home.vue";
 definePageMeta({
-  layout: 'landing'
-})
+  layout: "landing",
+});
 export default {
   name: "ProductSection",
   components: { CardsHome },
@@ -146,7 +205,8 @@ export default {
           originalPrice: "KES 3,500.00",
           available: 334,
           sold: 180,
-          image: "https://html.themexriver.com/radios/assets/img/product/img_52.png",
+          image:
+            "https://html.themexriver.com/radios/assets/img/product/img_52.png",
         },
         {
           name: "Wireless Speaker",
@@ -154,7 +214,8 @@ export default {
           originalPrice: "KES 5,500.00",
           available: 120,
           sold: 45,
-          image: "https://html.themexriver.com/radios/assets/img/product/img_53.png",
+          image:
+            "https://html.themexriver.com/radios/assets/img/product/img_53.png",
         },
         {
           name: "Smart Watch Pro",
@@ -162,14 +223,15 @@ export default {
           originalPrice: "KES 18,000.00",
           available: 80,
           sold: 25,
-          image: "https://html.themexriver.com/radios/assets/img/product/img_54.png",
+          image:
+            "https://html.themexriver.com/radios/assets/img/product/img_54.png",
         },
       ],
       features: [
         {
           icon: "pi pi-truck",
           title: "Free Shipping",
-          description: "Free shipping over $100",
+          description: "Free shipping over KES 100,000",
         },
         {
           icon: "pi pi-lock",
@@ -197,14 +259,14 @@ export default {
   methods: {
     getSeverity(status) {
       switch (status) {
-        case 'INSTOCK':
-          return 'success';
+        case "INSTOCK":
+          return "success";
 
-        case 'LOWSTOCK':
-          return 'warn';
+        case "LOWSTOCK":
+          return "warn";
 
-        case 'OUTOFSTOCK':
-          return 'danger';
+        case "OUTOFSTOCK":
+          return "danger";
 
         default:
           return null;
@@ -217,13 +279,12 @@ export default {
       const currentIndex = this.images.indexOf(this.selectedImage);
       this.selectedImage =
         this.images[
-        (currentIndex - 1 + this.images.length) % this.images.length
+          (currentIndex - 1 + this.images.length) % this.images.length
         ];
     },
     nextImage() {
       const currentIndex = this.images.indexOf(this.selectedImage);
-      this.selectedImage =
-        this.images[(currentIndex + 1) % this.images.length];
+      this.selectedImage = this.images[(currentIndex + 1) % this.images.length];
     },
   },
 };
@@ -232,7 +293,6 @@ export default {
 <style scoped>
 .product-section {
   /* background-color: #f9fafc; */
-
 }
 
 .feature__wrap {
