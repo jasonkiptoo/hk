5c3fb225b56c1e5e266acacb31c22481118108e1
jasonkiptoo <kiptoojason@gmail.com>
2025-02-05 07:10:22 +0300

    fixes

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,37 +1,41 @@
 <template>
-  <div class="w-full mx-auto h-screen flex items-center">
-    <div class="flex flex-wrap w-full mt-28">
-      <div class="hidden md:flex w-full md:w-1/2 items-center justify- mb-4 md:mb-0 image_cont">
-        <img src="@/assets/images/login.png" alt="Login" class="h-full" />
-      </div>
-      <div class="w-full md:w-1/2" style="
-          height: 100vh;
-          background-color: white;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          padding: 10px;
-        ">
-        <div>
-          <h2 class="text-5xl font-semibold mt-4 mb-8 text-left">
-            Log in to your account
-          </h2>
-          <div v-if="!isVerified">
-            <p class="text-gray-500 text-2xl mb-10 text-left">
-              Enter your login details below
-            </p>
-            <form @submit.prevent="login" class="w-full">
-              <input type="email" placeholder="Email or Phone Number"
+  <div class="">
+    <!-- <div class="hidden md:flex w-full md:w-1/2 items-center justify- mb-4 md:mb-0 image_cont">
+        <img src="@/assets/images/logoo.png" alt="Login" class="h-full" />
+      </div> -->
+    <div class="flex bg-red-200 w-full h-full flex-col items-center justify-center">
+      <img src="@/assets/images/logoo.png" alt="Login" class="h-28 w-28" />
+
+      <div v-if="!isVerified">
+        <p class="text-gray-500 text-2xl mb-10 text-center">
+          Welcome to Secure Group
+        </p>
+        <form @submit.prevent="login" class="w-full flex flex-col items-center">
+
+          <div class="flex flex-col gap-4 w-full bg-red-300">
+            <FloatLabel variant="on">
+              <InputText type="email" id="email" v-model="value3" />
+              <label for="email">Email</label>
+            </FloatLabel>
+
+            <FloatLabel variant="on">
+              <Password v-model="value" id="password" toggleMask />
+              <label for="password">Password</label>
+            </FloatLabel>
+          </div>
+
+
+          <!-- <input type="email" placeholder="Email or Phone Number"
                 class="md:w-full sm:w-full w-full border-b p-2 mb-4 focus:outline-none focus:border-red-500"
                 v-model="form.email" />
               <input type="password" placeholder="Password"
-                class="w-full border-b p-2 mb-4 focus:outline-none focus:border-red-500" v-model="form.password" />
-              <button type="submit" class="w-full bg-red-500 text-white py-3 rounded hover:bg-red-600 mt-12">
-                Log In
-              </button>
-            </form>
+                class="w-full border-b p-2 mb-4 focus:outline-none focus:border-red-500" v-model="form.password" /> -->
+          <button type="submit" class="w-full bg-red-500 text-white py-3 rounded hover:bg-red-600 mt-12">
+            Log In
+          </button>
+        </form>
 
-            <!-- <button
+        <!-- <button
               type="button"
               class="w-1/2 mt-4 border py-2 rounded flex justify-center items-center"
             >
@@ -42,33 +46,30 @@
               />
               Log in with Google
             </button> -->
-            <p class="text-sm mt-4 text-center">
-              Don't have an account?
-              <span class="text-blue-500 cursor-pointer" @click="$emit('toggle')">
-                Sign up
-              </span>
-            </p>
-          </div>
-          <div v-else>
-            <h3 class="text-3xl font-medium mb-4">Verify OTP</h3>
-            <p class="text-gray-500 mb-6">
-              Enter the OTP sent to your email address. <span class="font-weight-bold" style="font-weight: bold;">{{
-                form.email }} </span>
-            </p>
-            <div class="w-full mb-4 flex justify-center items-center">
-              <InputOtp :length="6" mask="*" v-model="otp" size="large" class="text-center p-3"
-                placeholder="Enter OTP" />
-            </div>
-
-            <button @click="verifyOtp" class="w-full bg-red-500 text-white py-3 rounded hover:bg-red-600">
-              Verify OTP
-            </button>
-          </div>
-        </div>
+        <p class="text-sm mt-4 text-center">
+          Don't have an account?
+          <span class="text-blue-500 cursor-pointer" @click="$emit('toggle')">
+            Sign up
+          </span>
+        </p>
       </div>
-      <Toast position="bottom-right" group="br" />
+      <div v-else>
+        <h3 class="text-3xl font-medium mb-4">Verify OTP</h3>
+        <p class="text-gray-500 mb-6">
+          Enter the OTP sent to your email address. <span class="font-weight-bold" style="font-weight: bold;">{{
+            form.email }} </span>
+        </p>
+        <div class="w-full mb-4 flex justify-center items-center">
+          <InputOtp :length="6" mask="*" v-model="otp" size="large" class="text-center p-3" placeholder="Enter OTP" />
+        </div>
+
+        <button @click="verifyOtp" class="w-full bg-red-500 text-white py-3 rounded hover:bg-red-600">
+          Verify OTP
+        </button>
+      </div>
     </div>
   </div>
+  <Toast position="bottom-right" group="br" />
 </template>
 
 <script>
